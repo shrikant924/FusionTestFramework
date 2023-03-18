0787a317b36d3e4d461b4663c52e9a7856e521d5
Shrikant lohar <shrikantlohar91@gmail.com>
2023-03-18 20:27:51 +0530

    appium log added

--- src/MobileTestLibrary/SetupUtils.py
+++ src/MobileTestLibrary/SetupUtils.py
@@ -1,5 +1,8 @@
+from datetime import datetime
 from appium.webdriver.appium_service import AppiumService, DEFAULT_PORT
 from robot.api.deco import keyword, library
+
+appium_service = AppiumService()
 
 
 @library
@@ -7,39 +10,19 @@ class SetupUtils:
 
     @keyword
     def start_appium_service(self):
-        appium_service = AppiumService()
+        appiumLogFileName = datetime.now().strftime('%d-%m-%y_%H-%M-%S')
+        path = '/FusionTestFramework/Log/' + 'appiumLog_' + appiumLogFileName + '.log'
         appium_service.start(
-             args=['--address', '127.0.0.1', '-p', str(DEFAULT_PORT), '--base-path', '/wd/hub/'] )
-                
+            args=['--address', '127.0.0.1', '-p', str(DEFAULT_PORT), '--base-path', '/wd/hub/', '--log', path ,'--log-level', 
+                  'debug' ,'--local-timezone'])
+
         assert appium_service.is_running
         assert appium_service.is_listening
 
     @keyword
     def stop_appium_service(self):
-        appium_service = AppiumService()
         appium_service.stop()
         assert (not appium_service.is_running)
         assert (not appium_service.is_listening)
 
-    # @keyword
-    # def stop_emulator(self):
-    #
-    #     try:
-    #         print('closing emulator...')
-    #         time.sleep(15.0)
-    #         os.system('adb -s emulator-5554 emu kill')
-    #         print('emulator closed successfully...')
-    #
-    #     except:
-    #         raise print("No emulator running...")
-
-    # @keyword
-    # def start_emulator(self, emulator_name, deviceType):
-    #     if deviceType == 'virtual':
-    #         print('Starting emulator...')
-    #         subprocess.Popen(['emulator', '-avd', emulator_name])
-    #         os.system('adb wait-for-device')
-    #         os.system('Perform whatever adb commands you need')
-    #         print('Started emulator...')
-    #     else:
-    #         print("Testing started")
+    
