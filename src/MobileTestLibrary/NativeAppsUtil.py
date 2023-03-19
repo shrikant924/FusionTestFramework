8e92f9a3d6e34b2ac46b406e5d7764051232792f
Shrikant lohar <shrikantlohar91@gmail.com>
2023-03-19 17:20:17 +0530

    .

--- src/MobileTestLibrary/NativeAppsUtil.py
+++ src/MobileTestLibrary/NativeAppsUtil.py
@@ -8,7 +8,8 @@ class NativeAppsUtil(ExtendedAppiumLibrary):
 
     @keyword
     def press_enter_key(self):
-        self.appium_library_instance.press_keycode(66)
+        self.press_keycode(66)
+        
 
     @keyword
     def unlock_device(self):
